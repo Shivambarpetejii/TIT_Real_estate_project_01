938fdae244ac438f5990de85dce2d984efafa2ca
Shivam_Barpete <118362195+Shivambarpetejii@users.noreply.github.com>
2023-11-19 12:53:33 +0530

    Add files via upload

---
+++ TIT-PROJECT-01/js/script.js
@@ -0,0 +1,28 @@
+let menu = document.querySelector('.header .menu');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   menu.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menu.classList.remove('active');
+}
+
+document.querySelectorAll('input[type="number"]').forEach(inputNumber => {
+   inputNumber.oninput = () =>{
+      if(inputNumber.value.length > inputNumber.maxLength) inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
+   };
+});
+
+document.querySelectorAll('.view-property .details .thumb .small-images img').forEach(images =>{
+   images.onclick = () =>{
+      src = images.getAttribute('src');
+      document.querySelector('.view-property .details .thumb .big-image img').src = src;
+   }
+});
+
+document.querySelectorAll('.faq .box-container .box h3').forEach(headings =>{
+   headings.onclick = () =>{
+      headings.parentElement.classList.toggle('active');
+   }
+});
